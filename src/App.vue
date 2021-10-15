d95928f971f582e76a9010beda672a9253e31e36
lollo03 <03lollo@gmail.com>
2021-10-15 20:52:18 +0200

    Aggiunta sezione "portfolio"

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <suspense>
     <div v-bind:class="{ dark: isDark }">
-      <div class="bg-yellow-50 dark:bg-gray-900 dark:text-white md:h-screen md:overflow-hidden">
-        <Top @darkToggle="darkToggle" :isDark="isDark"/>
+      <div
+        class="bg-yellow-50 dark:bg-gray-900 dark:text-white md:h-screen md:overflow-hidden"
+      >
+        <Top @darkToggle="darkToggle" :isDark="isDark" />
         <div class="md:flex h-full">
           <div class="md:w-1/3">
             <Scheda />
@@ -10,7 +12,24 @@
           <div class="md:h-100" style="border-left: 2px solid"></div>
           <hr class="md:hidden" />
           <div class="md:w-2/3 md:overflow-y-scroll h-full">
-            <Articolo />
+            <div class="w-100 h-10 border-b-2 flex">
+              <div
+                class="flex-auto border-r-2 flex justify-items-center items-center"
+                v-bind:class="{ sel: isPortfolio }"
+                @click="portfolio"
+              >
+                <p class="flex-auto">Portfolio</p>
+              </div>
+              <div
+                class="flex-auto flex justify-items-center	items-center"
+                v-bind:class="{ sel: !isPortfolio }"
+                @click="posts"
+              >
+                <p class="flex-auto">Posts</p>
+              </div>
+            </div>
+            <Articolo v-if="!isPortfolio" />
+            <Portfolio v-else />
           </div>
         </div>
       </div>
@@ -22,6 +41,7 @@
 import Articolo from "./components/Articolo.vue";
 import Top from "./components/Top.vue";
 import Scheda from "./components/Scheda.vue";
+import Portfolio from "./components/Portfolio.vue";
 import { ref } from "vue";
 
 export default {
@@ -30,20 +50,23 @@ export default {
     Articolo,
     Top,
     Scheda,
+    Portfolio,
   },
   setup() {
     const isDark = ref(false);
+    const isPortfolio = ref(false);
     return {
       isDark,
+      isPortfolio,
     };
   },
   mounted() {
-    this.isDark = this.getMediaPreference()
+    this.isDark = this.getMediaPreference();
   },
   methods: {
     darkToggle() {
       this.isDark = !this.isDark;
-      localStorage.isDark = this.isDark
+      localStorage.isDark = this.isDark;
     },
     getMediaPreference() {
       const hasDarkPreference = window.matchMedia(
@@ -55,6 +78,12 @@ export default {
         return false;
       }
     },
+    portfolio() {
+      this.isPortfolio = true;
+    },
+    posts() {
+      this.isPortfolio = false;
+    },
   },
 };
 </script>
@@ -65,5 +94,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+
+.sel {
+  @apply text-purple-400;
+  @apply font-bold;
 }
 </style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my-5 mx-8 text-justify dark:arancio">
+    <h1>Sezione in costruzione</h1>
+    <h2>torna più tardi...</h2>
+
+    <br />
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Portfolio",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.arancio {
+  color: orange;
+}
+</style>
