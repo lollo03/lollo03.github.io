c43ec81e07dc90aef36c374d7c01adf69f5a9103
lollo03 <03lollo@gmail.com>
2023-08-20 10:15:31 +0200

    Small style fixed: added button for Yathzee board on navbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div v-bind:class="{ dark: isDark }">
-    <div
-      class="bg-yellow-50 dark:bg-gray-900 dark:text-white md:h-screen md:overflow-hidden"
-    >
+    <div class="bg-yellow-50 dark:bg-gray-900 dark:text-white md:h-screen md:overflow-hidden">
       <Top @darkToggle="darkToggle" :isDark="isDark" />
       <div class="md:flex h-full">
-        <div class="md:w-1/3">
+        <div
+          class="md:w-1/3"
+          style="overflow: scroll; margin-bottom: 3em; -ms-overflow-style: none;
+  scrollbar-width: none;"
+        >
           <Scheda />
         </div>
         <div class="md:h-100" style="border-left: 2px solid"></div>
         <hr class="md:hidden" />
         <div class="md:w-2/3 md:overflow-y-scroll h-full">
           <div class="w-100 h-10 border-b-2 border-t-2 flex">
-            <div
-              class="flex-auto border-r-2 flex justify-items-center items-center cursor-pointer"
-              v-bind:class="{ sel: isPortfolio }"
-              @click="portfolio"
-            >
+            <div class="flex-auto border-r-2 flex justify-items-center items-center cursor-pointer" v-bind:class="{ sel: isPortfolio }" @click="portfolio">
               <p class="flex-auto">Portfolio</p>
             </div>
-            <div
-              class="flex-auto flex justify-items-center	items-center cursor-pointer"
-              v-bind:class="{ sel: !isPortfolio }"
-              @click="posts"
-            >
+            <div class="flex-auto flex justify-items-center	items-center cursor-pointer" v-bind:class="{ sel: !isPortfolio }" @click="posts">
               <p class="flex-auto">Posts</p>
             </div>
           </div>
@@ -33,23 +27,17 @@
             </template>
             <template #fallback>
               <div class="flex">
-                <div
-                  style="border-top-color:transparent"
-                  class="w-16 h-16 border-4 border-purple-400 border-dotted rounded-full animate-spin m-auto mt-12"
-                ></div>
+                <div style="border-top-color:transparent" class="w-16 h-16 border-4 border-purple-400 border-dotted rounded-full animate-spin m-auto mt-12"></div>
               </div>
             </template>
           </suspense>
           <suspense v-if="isPortfolio">
             <template #default>
-              <Portfolio />
+              <Portfolio style="margin-bottom: 8em;" />
             </template>
             <template #fallback>
               <div class="flex">
-                <div
-                  style="border-top-color:transparent"
-                  class="w-16 h-16 border-4 border-purple-400 border-dotted rounded-full animate-spin m-auto mt-12"
-                ></div>
+                <div style="border-top-color:transparent" class="w-16 h-16 border-4 border-purple-400 border-dotted rounded-full animate-spin m-auto mt-12"></div>
               </div>
             </template>
           </suspense>
@@ -91,9 +79,7 @@ export default {
       localStorage.isDark = this.isDark;
     },
     getMediaPreference() {
-      const hasDarkPreference = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
+      const hasDarkPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
       if (hasDarkPreference) {
         return true;
       } else {
