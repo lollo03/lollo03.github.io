0b8c285115337a369224d7ff9ab96fe46b7cec49
lollo03 <03lollo@gmail.com>
2024-02-23 14:24:00 +0100

    Removed blog

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,21 @@
     <div class="bg-yellow-50 dark:bg-gray-900 dark:text-white md:h-screen md:overflow-hidden">
       <Top @darkToggle="darkToggle" :isDark="isDark" />
       <div class="md:flex h-full">
-        <div class="md:w-1/3 hide-scrollbar" style="overflow: scroll; margin-bottom: 3em;">
+        <div class="md:w-1/3 hide-scrollbar" style="overflow: scroll; margin-bottom: 3em">
           <suspense> <Scheda /> </suspense>
         </div>
         <div class="md:h-100" style="border-left: 2px solid"></div>
         <hr class="md:hidden" />
         <div class="md:w-2/3 md:overflow-y-scroll h-full">
           <div class="w-100 h-10 border-b-2 border-t-2 flex">
-            <div class="flex-auto border-r-2 flex justify-items-center items-center cursor-pointer" v-bind:class="{ sel: isPortfolio }" @click="portfolio">
+            <div class="flex-auto border-r-2 flex justify-items-center items-center cursor-pointer">
               <p class="flex-auto">Portfolio</p>
             </div>
-            <div class="flex-auto flex justify-items-center items-center cursor-pointer" v-bind:class="{ sel: !isPortfolio }" @click="posts">
-              <p class="flex-auto">Posts</p>
+            <div class="flex-auto flex justify-items-center items-center cursor-pointer">
+              <a class="flex-auto" href="https://www.lolloandr.com/blog">Blog</a>
             </div>
           </div>
-          <suspense v-if="!isPortfolio">
-            <template #default>
-              <Articolo />
-            </template>
-            <template #fallback>
-              <div class="flex">
-                <div style="border-top-color: transparent" class="w-16 h-16 border-4 border-purple-400 border-dotted rounded-full animate-spin m-auto mt-12"></div>
-              </div>
-            </template>
-          </suspense>
-          <suspense v-if="isPortfolio">
+          <suspense>
             <template #default>
               <Portfolio style="margin-bottom: 8em" />
             </template>
@@ -44,7 +34,6 @@
 </template>
 
 <script>
-import Articolo from "./components/Articolo.vue";
 import Top from "./components/Top.vue";
 import Scheda from "./components/Scheda.vue";
 import Portfolio from "./components/Portfolio.vue";
@@ -53,17 +42,14 @@ import { ref } from "vue";
 export default {
   name: "App",
   components: {
-    Articolo,
     Top,
     Scheda,
     Portfolio,
   },
   setup() {
     const isDark = ref(false);
-    const isPortfolio = ref(true);
     return {
       isDark,
-      isPortfolio,
     };
   },
   mounted() {
@@ -81,12 +67,6 @@ export default {
       } else {
         return false;
       }
-    },
-    portfolio() {
-      this.isPortfolio = true;
-    },
-    posts() {
-      this.isPortfolio = false;
     },
   },
 };
