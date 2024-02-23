a48afecf3ea2238bb94d355b312186ca56c2e1eb
lollo03 <03lollo@gmail.com>
2024-02-23 14:26:17 +0100

    removed useless functions and updated sitemap

--- src/api.js
+++ src/api.js
@@ -1,37 +1,7 @@
 const brach = "articles";
 const repo = "lollo03.github.io";
-const IgnoredFiles = ['portfolio.md', 'aboutme.md']
 
 
-export async function ottieniArticoli() {
-  let response = await fetch(`https://api.github.com/repos/lollo03/${repo}/git/trees/${brach}?recursive=1`);
-  let temp = await response.json();
-  let arr = temp.tree;
-  let fin = [];
-  arr.forEach((el) => {
-    if (el.path.split(".")[1] == "md" && !el.path.startsWith(".") && !IgnoredFiles.includes(el.path)) {
-      //solo gli articoli vanno di qua
-      fin.push(el.path);
-    }
-  });
-  return fin;
-}
-
-export async function ottieniArticolo(nome) {
-  let response = await fetch(`https://raw.githubusercontent.com/lollo03/${repo}/${brach}/${nome}`);
-  let temp = {};
-  let risp = await response.text();
-
-  let t = risp.split("<!--")[1];
-  t = t.split("-->")[0];
-
-  let obj = JSON.parse(t);
-  temp.id = obj.nome;
-  temp.nome = obj.nome;
-  temp.desc = obj.desc;
-  temp.testo = risp;
-  return temp;
-}
 
 export async function ottieniPortfolio() {
   let response = await fetch(`https://raw.githubusercontent.com/lollo03/${repo}/${brach}/portfolio.md`);
